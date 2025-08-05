444076884d6b075f8d90346ef0f7fc5c6909e3c1
vinicius <vinnicius4@hotmail.com>
2025-08-05 17:36:46 -0300

    alterando o ref para a montagem correta da aplicação no deploy

--- src/components/motion/container.tsx
+++ src/components/motion/container.tsx
@@ -1,76 +1,84 @@
 import * as motion from "motion/react-client";
-import { forwardRef, ReactNode } from "react";
+import { forwardRef, ReactNode, ForwardedRef } from "react";
 import { HTMLMotionProps } from "motion/react";
 
 type CustomProps = {
-  children?: ReactNode
-  delay?: number
-  duration?: number
-  amount?: number | "some" | "all"
+  children?: ReactNode;
+  delay?: number;
+  duration?: number;
+  amount?: number | "some" | "all";
 };
 
 type SectionProps = CustomProps & HTMLMotionProps<"section">;
 
-export const SectionFadeInUp = forwardRef<HTMLDivElement, SectionProps>(
-  ({ children, duration = 1, delay = 0, ...rest }, ref) => {
-    return (
-      <motion.section
-        ref={ref}
-        initial='hide'
-        whileInView='show'
-        exit='hide'
-        variants={{
-          hide: {
-            opacity: 0,
-            y: 50,
+const SectionFadeInUpComponent = (
+  { children, duration = 1, delay = 0, ...rest }: SectionProps,
+  ref: ForwardedRef<HTMLDivElement>
+) => {
+  return (
+    <motion.section
+      ref={ref}
+      initial='hide'
+      whileInView='show'
+      exit='hide'
+      variants={{
+        hide: {
+          opacity: 0,
+          y: 50,
+        },
+        show: {
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration,
+            delay,
           },
-          show: {
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration,
-              delay
-            },
-          },
-        }}
-        viewport={{ once: true, amount: 0 }}
-        {...rest}
-      >
-        {children}
-      </motion.section>
-    );
-  }
-);
+        },
+      }}
+      viewport={{ once: true, amount: 0 }}
+      {...rest}
+    >
+      {children}
+    </motion.section>
+  );
+};
+export const SectionFadeInUp = forwardRef(SectionFadeInUpComponent);
+SectionFadeInUp.displayName = "SectionFadeInUp";
+
 
 type DivProps = CustomProps & HTMLMotionProps<"div">;
 
-export const DivFadeInUp = forwardRef<HTMLDivElement, DivProps>(
-  ({ children, duration = 1, delay = 0, amount = 'all', ...rest }, ref) => {
-    return (
-      <motion.div
-        ref={ref}
-        initial='hide'
-        whileInView='show'
-        exit='hide'
-        variants={{
-          hide: {
-            opacity: 0,
-            y: 50,
+const DivFadeInUpComponent = (
+  { children, duration = 1, delay = 0, amount = 'all', ...rest }: DivProps,
+  ref: ForwardedRef<HTMLDivElement>
+) => {
+  return (
+    <motion.div
+      ref={ref}
+      initial='hide'
+      whileInView='show'
+      exit='hide'
+      variants={{
+        hide: {
+          opacity: 0,
+          y: 50,
+        },
+        show: {
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration,
+            delay,
           },
-          show: {
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration,
-              delay
-            },
-          },
-        }}
-        viewport={{ once: true, amount }}
-        {...rest}
-      >
-        {children && children}
-      </motion.div>
-    );
-  }
-);
+        },
+      }}
+      viewport={{ once: true, amount }}
+      {...rest}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const DivFadeInUp = forwardRef(DivFadeInUpComponent);
+DivFadeInUp.displayName = "DivFadeInUp";
